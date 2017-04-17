c1764f523194c4bbb7e038272c3a4859f157c68a
adrianmg <yensamg@gmail.com>
2017-04-17 08:56:39 -0700

    Added ground base to start working on Paddle movement

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    private Rigidbody2D paddleRigidBody;
+
+    void Start () {
+
+        paddleRigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+        Debug.Log(paddleRigidBody);
+		
+	}
+}
