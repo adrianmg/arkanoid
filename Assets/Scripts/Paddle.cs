96cc1cc2e9554845eed16da5bd29d04149c80947
adrianmg <yensamg@gmail.com>
2017-04-18 08:24:16 -0700

    Added Paddle control by keys

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,14 +1,39 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class Paddle : MonoBehaviour {
+public class Paddle : MonoBehaviour
+{
+    private float           speed = 14.0f;
+    private Rigidbody2D     paddleRigidBody;
+    private Vector2         paddleMove;
 
-    private Rigidbody2D paddleRigidBody;
-
-    void Start () {
-
+    void Start ()
+    {
         paddleRigidBody = gameObject.GetComponent<Rigidbody2D>();
-
-        Debug.Log(paddleRigidBody);
-		
 	}
+
+    void Update()
+    {
+        // Keys movement
+        paddleMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        // Mouse 'push' movement
+        //paddleMove = new Vector2(Input.GetAxisRaw("Mouse X"), 0);
+        // Mouse position movement
+        //paddleMove = new Vector2(Input.mousePosition.x, 0);
+
+        Debug.Log(Input.GetAxisRaw("Mouse X"));
+        Debug.Log(Input.mousePosition.x);
+    }
+
+    private void FixedUpdate()
+    {
+        paddleRigidBody.velocity = paddleMove * speed;
+
+        CheckPaddleLimit();
+    }
+
+    void CheckPaddleLimit()
+    {
+        Debug.Log(paddleRigidBody.position.x);
+    }
 }
