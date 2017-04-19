3b1e82a14d5440ca6a5e83b96bf8bfc9a4380061
adrianmg <yensamg@gmail.com>
2017-04-19 08:40:00 -0700

    Paddle is now controlled by mouse
    Need to integrate keyboards back and clean up how the units and
    positions are calculated instead of magic numbers hard coded.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour
 {
-    private float           speed = 14.0f;
-    private Rigidbody2D     paddleRigidBody;
-    private Vector2         paddleMove;
+    private float speed = 14.0f;
+    private Rigidbody2D paddleRigidBody;
+    private Vector2 paddleMove;
+    private float boundaryLeft = 1.5f;
+    private float boundaryRight= 21.0f;
 
     void Start ()
     {
         paddleRigidBody = gameObject.GetComponent<Rigidbody2D>();
-	}
+
+        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
+
+    }
 
     void Update()
     {
@@ -21,8 +27,9 @@ public class Paddle : MonoBehaviour
         // Mouse position movement
         //paddleMove = new Vector2(Input.mousePosition.x, 0);
 
-        Debug.Log(Input.GetAxisRaw("Mouse X"));
-        Debug.Log(Input.mousePosition.x);
+        //Debug.Log(Input.GetAxisRaw("Mouse X"));
+
+        CheckMouseMovement();
     }
 
     private void FixedUpdate()
@@ -34,6 +41,25 @@ public class Paddle : MonoBehaviour
 
     void CheckPaddleLimit()
     {
-        Debug.Log(paddleRigidBody.position.x);
+        //Debug.Log(paddleRigidBody.position.x);
+    }
+
+    void CheckMouseMovement()
+    {
+        float horizontalUnits = Camera.main.orthographicSize * 2;
+        float horizontalUnitsPaddle = Screen.width / 16.0f;
+
+        float mousePositionUnit = (Input.mousePosition.x / Screen.width) * horizontalUnits;
+        mousePositionUnit = Mathf.Clamp(mousePositionUnit, 0, horizontalUnits);
+
+        Debug.Log("Scenario tile:" + mousePositionUnit);
+        Debug.Log("Paddle tile:" + (Input.mousePosition.x / Screen.width) * horizontalUnitsPaddle);
+
+
+
+        float mouseUnitPosition = Input.mousePosition.x / Screen.width * 24;
+        mouseUnitPosition = Mathf.Clamp(mouseUnitPosition, boundaryLeft, boundaryRight);
+
+        paddleRigidBody.transform.position = new Vector3(mouseUnitPosition, this.transform.position.y, paddleRigidBody.transform.position.z);
     }
 }
