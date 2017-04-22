0567dc1ad5c3b0b90a36cd0ae5cf1a3520eae2d0
adrianmg <yensamg@gmail.com>
2017-04-22 08:31:51 -0700

    Added support to cap the FPS rate

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour {
+
+	void Awake() {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+	
+}
