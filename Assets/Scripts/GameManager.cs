e9edddd2f17b31e43f2a480e6ea9ae08cac7d3d8
adrianmg <yensamg@gmail.com>
2017-04-22 20:57:42 -0700

    Pressing 'R' reloads the level
    For developing purposes (just in the editor)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private void Update()
+    {
+        #if UNITY_EDITOR
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        #endif
+    }
+}
