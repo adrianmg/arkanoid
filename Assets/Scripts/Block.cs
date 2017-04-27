809c621af95386c4237f7c7a4d1444925aae90b0
adrianmg <yensamg@gmail.com>
2017-04-27 08:31:42 -0700

    Added Block logic: collision, destroy and audio

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int hits;
+
+    private AudioSource audioDestroy;
+
+    private void Awake()
+    {
+        AddAudioDestroy();
+    }
+
+    private void OnCollisionEnter2D(Collision2D e)
+    {
+        if (e.gameObject.tag == "Ball")
+        {
+            hits--;
+
+            Debug.Log("Hits: " + hits);
+
+            if (hits <= 0)
+            {
+                DestroyBlock();
+            }
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(audioDestroy.clip, this.transform.position, 1.0f);
+
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+
+    private void AddAudioDestroy()
+    {
+        audioDestroy = gameObject.AddComponent<AudioSource>();
+
+        audioDestroy.clip = Resources.Load<AudioClip>("block-" + Random.Range(1, 3).ToString());
+
+        Debug.Log(audioDestroy.clip);
+    }
+}
