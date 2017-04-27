b9834ffb4c54a8c2aa2789db0fbc69d882e040c7
adrianmg <yensamg@gmail.com>
2017-04-27 08:35:07 -0700

    Added custom interface to impot from Tiled to Unity
    Now blocks are imported tailored for the game. Property hit added.

---
+++ Assets/Scripts/Editor/CustomTiledImporterForBlocks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEditor;
+using UnityEngine;
+
+[Tiled2Unity.CustomTiledImporter]
+class CustomTiledImporterForBlocks : Tiled2Unity.ICustomTiledImporter
+{
+    int layerBlock = LayerMask.NameToLayer("Block");
+
+    public void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> customProperties)
+    {
+
+        if (layerBlock > 0 && gameObject.layer == layerBlock)
+        {
+            gameObject.name = "BlockTile";
+
+            GameObject collider = gameObject.GetComponentInChildren<Collider2D>().gameObject;
+            collider.name = "Block";
+            Block block = collider.AddComponent<Block>();
+            block.hits = Int16.Parse(customProperties["hits"]);
+        }
+    }
+
+    public void CustomizePrefab(GameObject prefab)
+    {
+        // Do nothing
+    }
+}
+
