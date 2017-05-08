5f5c61e85841cc6cf4c4dd583a02628604e582db
adrianmg <yensamg@gmail.com>
2017-05-08 07:02:43 -0700

    Improvements on importer of levels from Tiled
    Now also imports score value apart from hits and it's more solid and
    prevent errors.

--- Assets/Scripts/Editor/CustomTiledImporterForBlocks.cs
+++ Assets/Scripts/Editor/CustomTiledImporterForBlocks.cs
@@ -7,7 +7,7 @@ using UnityEditor;
 using UnityEngine;
 
 [Tiled2Unity.CustomTiledImporter]
-class CustomTiledImporterForBlocks : Tiled2Unity.ICustomTiledImporter
+public class CustomTiledImporterForBlocks : Tiled2Unity.ICustomTiledImporter
 {
     int layerBlock = LayerMask.NameToLayer("Block");
 
@@ -16,12 +16,31 @@ class CustomTiledImporterForBlocks : Tiled2Unity.ICustomTiledImporter
 
         if (layerBlock > 0 && gameObject.layer == layerBlock)
         {
+
+            // Renaming
             gameObject.name = "BlockTile";
 
             GameObject collider = gameObject.GetComponentInChildren<Collider2D>().gameObject;
             collider.name = "Block";
             Block block = collider.AddComponent<Block>();
-            block.hits = Int16.Parse(customProperties["hits"]);
+
+            // Hits value
+            block.Hits = GameManager.BlockHitsBase;
+            string hits;
+
+            if (customProperties.TryGetValue("hits", out hits))
+            {
+                block.Hits = Int16.Parse(customProperties["hits"]);
+            }
+
+            // Score value
+            block.Score = GameManager.BlockScoreBase;
+            string score;
+
+            if (customProperties.TryGetValue("score", out score))
+            {
+                block.Score = Int16.Parse(customProperties["score"]);
+            }
         }
     }
 
