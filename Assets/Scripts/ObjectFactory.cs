ce5d4d3745d69485d9a6c40cf9c3206e934bde69
adrianmg <yensamg@gmail.com>
2017-05-08 06:58:07 -0700

    Added Object factory

---
+++ Assets/Scripts/ObjectFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFactory : MonoBehaviour
+{
+    protected static ObjectFactory instance;
+
+    public GameObject BlockExplosion;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static BlockExplosion CreateBlockExplosion(Transform transform)
+    {
+        var explosion = Object.Instantiate(instance.BlockExplosion, transform.position, Quaternion.identity).GetComponent<BlockExplosion>();
+        explosion.Initialize();
+
+        return explosion;
+    }
+}
